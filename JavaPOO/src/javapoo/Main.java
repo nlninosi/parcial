43204d5e3a59b864e9cbe7dafc4939196de5c772
leotalorac <lwotalorac@unal.edu.co>
2017-08-15 15:34:15 -0500

    v 1.0

---
+++ JavaPOO/src/javapoo/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapoo;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Main {
+    public static void main(String [] args){
+        Canción c1 = new Canción();
+        Canción c2 = new Canción();
+        c1.titulo = "Close to Me";
+        c2.titulo = "Cancion 2";
+        c1.sonar();
+        c2.sonar();
+        
+    }
+    
+}
